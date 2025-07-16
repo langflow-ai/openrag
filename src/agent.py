51a66582d0067fb45e9be0e6e211a6dc855d452f
estevez.sebastian@gmail.com <estevezsebastian@gmail.com>
2025-07-16 01:18:29 -0400

    responses api (not streaming yet)

--- src/agent.py
+++ src/agent.py
@@ -1,23 +1,17 @@
-import asyncio
-
-from agentd.patch import patch_openai_with_mcp
-
 messages = [{"role": "system", "content": "You are a helpful assistant. use your tools to answer questions."}]
 
 # Async version for web server
-async def async_chat(async_client, prompt: str) -> str:
+async def async_chat(async_client, prompt: str, model: str = "gpt-4.1-mini", previous_response_id: str = None) -> str:
     global messages
     messages += [{"role": "user", "content": prompt}]
-    response = await async_client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=messages,
-        mcp_strict=True
+    response = await async_client.responses.create(
+        model=model,
+        input=prompt,
+        previous_response_id=previous_response_id,
     )
 
-    response_text = response.choices[0].message.content
+    response_id = response.id
+    response_text = response.output_text
     print(f"user ==> {prompt}")
     print(f"agent ==> {response_text}")
     return response_text
-
-if __name__ == "__main__":
-    asyncio.run(async_chat("What pods are there?"))
