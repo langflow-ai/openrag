aa5067865f58f68fbb59c3f926df4db53c841f6b
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-09-18 16:43:10 -0300

    implemented label wrapper

--- frontend/components/label-input.tsx
+++ frontend/components/label-input.tsx
@@ -1,7 +1,5 @@
-import { Info } from "lucide-react";
+import { LabelWrapper } from "./label-wrapper";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export function LabelInput({
   label,
@@ -16,25 +14,13 @@ export function LabelInput({
   required: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <div className="space-y-2">
-      <Label
-        htmlFor={id}
-        className="text-mmd font-medium flex items-center gap-1"
-      >
-        {label}
-        {required && <span className="text-red-500">*</span>}
-        {helperText && (
-          <Tooltip>
-            <TooltipTrigger>
-              <Info className="w-3.5 h-3.5 text-muted-foreground" />
-            </TooltipTrigger>
-            <TooltipContent>{helperText}</TooltipContent>
-          </Tooltip>
-        )}
-      </Label>
-      <div className="relative">
-        <Input id={id} {...props} />
-      </div>
-    </div>
+    <LabelWrapper
+      label={label}
+      helperText={helperText}
+      id={id}
+      required={required}
+    >
+      <Input id={id} {...props} />
+    </LabelWrapper>
   );
 }
