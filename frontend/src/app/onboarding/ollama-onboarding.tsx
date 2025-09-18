e6d88b178036a29c8069f22831fe00d02643f75b
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2025-09-18 16:43:23 -0300

    added model on ollama onboarding

--- frontend/src/app/onboarding/ollama-onboarding.tsx
+++ frontend/src/app/onboarding/ollama-onboarding.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { LabelInput } from "@/components/label-input";
+import { LabelWrapper } from "@/components/label-wrapper";
+import OllamaLogo from "@/components/logo/ollama-logo";
 import type { Settings } from "../api/queries/useGetSettingsQuery";
 import { AdvancedOnboarding } from "./advanced";
+import { ModelSelector } from "./model-selector";
 
 export function OllamaOnboarding({
   settings,
@@ -9,6 +13,16 @@ export function OllamaOnboarding({
   settings: Settings;
   setSettings: (settings: Settings) => void;
 }) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const frameworks = [
+    { value: "gpt-oss", label: "gpt-oss", default: true },
+    { value: "llama3.1", label: "llama3.1" },
+    { value: "llama3.2", label: "llama3.2" },
+    { value: "llama3.3", label: "llama3.3" },
+    { value: "llama3.4", label: "llama3.4" },
+    { value: "llama3.5", label: "llama3.5" },
+  ];
   return (
     <>
       <LabelInput
@@ -18,6 +32,20 @@ export function OllamaOnboarding({
         required
         placeholder="http://..."
       />
+
+      <LabelWrapper
+        label="Embedding Model"
+        helperText="The embedding model for your Ollama server."
+        id="embedding-model"
+        required={true}
+      >
+        <ModelSelector
+          options={frameworks}
+          icon={<OllamaLogo className="w-4 h-4" />}
+          value={value}
+          onValueChange={setValue}
+        />
+      </LabelWrapper>
 
       <AdvancedOnboarding modelProvider="ollama" />
     </>
