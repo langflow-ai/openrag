2a1f2ae21cffafefc968d87bb1f4996ceb378c1b
Brent O'Neill <brent.oneill@datastax.com>
2025-10-02 08:57:38 -0600

    fix bad merge

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -9,11 +9,7 @@ const Card = React.forwardRef<
     ref={ref}
     className={cn(
       "rounded-xl border border-border bg-card text-card-foreground shadow-sm",
-<<<<<<< HEAD
       className
-=======
-      className,
->>>>>>> main
     )}
     {...props}
   />
