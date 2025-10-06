b3a11fed39aaff0e313f0062c52a97023d9d7bdd
Brent O'Neill <brent.oneill@datastax.com>
2025-10-06 14:36:21 -0600

    fix number input border colors

--- frontend/components/ui/inputs/number-input.tsx
+++ frontend/components/ui/inputs/number-input.tsx
@@ -37,28 +37,34 @@ export const NumberInput = ({
           onChange={(e) => onChange(parseInt(e.target.value) || 0)}
           className="w-full pr-20 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
         />
-        <div className="absolute inset-y-0 right-0 flex items-center">
+        <div className="absolute inset-y-0 right-0 top-0 p-[1px] py-[1.5px] flex items-center ">
           <span className="text-sm text-placeholder-foreground mr-4 pointer-events-none">
             {unit}
           </span>
-          <div className="flex flex-col">
+          <div className="flex flex-col mt-[2px] mb-[2px]">
             <Button
               aria-label={`Increase ${label} value`}
-              className="h-5 rounded-l-none rounded-br-none border-input border-b-[0.5px] focus-visible:relative"
+              className="h-5 rounded-l-none rounded-br-none border-input border-t-transparent border-r-transparent border-b-[0.5px] hover:border-t-[.5px] hover:border-foreground"
               variant="outline"
               size="iconSm"
               onClick={() => onChange(value + 1)}
             >
-              <Plus className="text-muted-foreground" size={8} />
+              <Plus
+                className="text-muted-foreground hover:text-foreground"
+                size={8}
+              />
             </Button>
             <Button
               aria-label={`Decrease ${label} value`}
-              className="h-5 rounded-l-none rounded-tr-none border-input border-t-[0.5px] focus-visible:relative"
+              className="h-5 rounded-l-none rounded-tr-none border-input border-b-transparent border-r-transparent hover:border-b-1 hover:border-b-[.5px] hover:border-foreground"
               variant="outline"
               size="iconSm"
               onClick={() => onChange(value - 1)}
             >
-              <Minus className="text-muted-foreground" size={8} />
+              <Minus
+                className="text-muted-foreground hover:text-foreground"
+                size={8}
+              />
             </Button>
           </div>
         </div>
