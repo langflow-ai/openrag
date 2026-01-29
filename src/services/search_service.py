86a8d021ffbd459f959058ddcf24baeab1419b48
ajithreji <ajithvreji@ibm.com>
2026-01-29 11:37:34 +0530

    Update search_service.py with new index search

--- src/services/search_service.py
+++ src/services/search_service.py
@@ -329,9 +329,9 @@ class SearchService:
         search_body = {
             "query": query_block,
             "aggs": {
-                "data_sources": {"terms": {"field": "filename.keyword", "size": 20}},
+                "data_sources": {"terms": {"field": "filename", "size": 20}},
                 "document_types": {"terms": {"field": "mimetype", "size": 10}},
-                "owners": {"terms": {"field": "owner_name.keyword", "size": 10}},
+                "owners": {"terms": {"field": "owner", "size": 10}},
                 "connector_types": {"terms": {"field": "connector_type", "size": 10}},
                 "embedding_models": {"terms": {"field": "embedding_model", "size": 10}},
             },
