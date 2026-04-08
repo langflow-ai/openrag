f76d8ed67c0736276a0161b28452adb1629272b8
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2026-04-08 16:42:47 -0300

    updated documentation for file

--- src/config/embedding_constants.py
+++ src/config/embedding_constants.py
@@ -1,4 +1,4 @@
-"""Embedding model constants and dimensions."""
+"""Embedding model constants."""
 
 OPENAI_DEFAULT_EMBEDDING_MODEL = "text-embedding-3-small"
 OPENAI_EMBEDDING_MODEL_PREFIX = "text-embedding"
